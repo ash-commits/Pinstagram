4404f80b6eb11ea18ee17d2d2a09c817553ae72c
Ashok Kumar <ashokkumar200194@gmail.com>
2022-02-01 23:19:05 +0530

    23:18

--- vijaycommits/src/router/index.js
+++ vijaycommits/src/router/index.js
@@ -3,9 +3,10 @@ import Router from 'vue-router'
 import Home from '@/components/Home/Home.vue'
 import Register from '@/components/logs/Register.vue'
 import Login from '@/components/logs/Login.vue'
-import CreatePost from '@/components/Home/CreatePost.vue'
+//  import CreatePost from '@/components/Home/CreatePost.vue'
 import ProfileHome from '@/components/Profile/ProfileHome.vue'
 import NotificationHome from '@/components/Notification/NotificationHome'
+import EditProfile from '@/components/Profile/EditProfile'
 Vue.use(Router)
 
 export default new Router({
@@ -25,11 +26,11 @@ export default new Router({
       name: 'Register',
       component: Register
     },
-    {
-      path:'/create',
-      name:'CreatePost',
-      component:CreatePost
-    },
+    // {
+    //   path:'/create',
+    //   name:'CreatePost',
+    //   component:CreatePost
+    // },
     {
       path: '/profile',
       name: 'ProfileHome',
@@ -39,6 +40,11 @@ export default new Router({
       path: '/notification',
       name: 'NotificationHome',
       component: NotificationHome
+    },
+    {
+      path: '/edit',
+      name: 'EditProfile',
+      component: EditProfile
     }
   ]
 })
