ad08ef1563a69cf20453c0b885b01b1f0ca3586a
damarla vijay Sai nag <64904540+the-damarla@users.noreply.github.com>
2022-02-02 08:41:18 +0530

    08:41

--- vijaycommits/src/components/Profile/ProfileHome.vue
+++ vijaycommits/src/components/Profile/ProfileHome.vue
@@ -11,17 +11,22 @@
             <div class="col-auto">Your Posts</div>
             <div class="col"><hr></div>
         </div>
+        <div>
+            <YourPost />
+        </div>
     </div>
 </template>
 
 <script>
 import Profile from '@/components/Profile/Profile.vue'
 import NavBar from '@/components/Home/NavBar.vue'
+import YourPost from '@/components/Profile/YourPost.vue'
     export default{
         name: 'ProfileHome',
         components: {
             Profile,
-            NavBar
+            NavBar,
+            YourPost
         }
     }
 </script>
---
+++ vijaycommits/src/components/Profile/YourPost.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="Post">
+             <YourPostCard v-for='post in posts' :key="post.id" v-bind:post="post"/>
+        </div>
+</template>
+
+<script>
+import YourPostCard from '@/components/Profile/YourPostCard.vue'
+import axios from 'axios'
+export default {
+    name: 'YourPost',
+    components: {
+        YourPostCard
+    },
+    data()
+    {
+      return{
+          posts:[]
+      }  
+    },
+    methods: {
+        async fetchPosts(){
+            await axios.get('https://jsonplaceholder.typicode.com/photos').then((res)=> {this.posts = res.data}).catch(err=>console.log(err))
+        }
+    },
+    mounted() {
+        this.fetchPosts()
+    }
+}
+</script>
