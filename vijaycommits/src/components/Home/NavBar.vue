b40cea66ff4840d8d5e5461db5facfc6324625a7
Ashok Kumar <ashokkumar200194@gmail.com>
2022-02-04 12:08:43 +0530

    1208

--- vijaycommits/src/components/Home/NavBar.vue
+++ vijaycommits/src/components/Home/NavBar.vue
@@ -44,7 +44,40 @@
                           <button @click="onUpload()" style="border:transparent">Upload</button>
                         </div>                    
                     </form> -->
-                    
+                    <div>
+    <select v-model="user.type">
+        <option disabled value="">Please select one</option>
+        <!-- <option>Text</option> -->
+        <option>Image</option>
+        <option>Audio</option>
+        <option>Video</option>
+    </select>
+    <span>Selected: {{ user.type }}</span>
+    <hr />
+    <!-- <div >
+      <p>Upload an image to Firebase:</p>
+      <input type="file" @change="previewImage" >
+    </div> -->
+    <!-- <div>
+        <p>video</p>
+        <input type="file" @change="previewVideo" accept="video/*">
+    </div> -->
+    <!-- <div>
+      <p>Progress: {{uploadValue.toFixed()+"%"}}
+      <progress id="progress" :value="uploadValue" max="100" ></progress>  </p>
+    </div>
+    <div v-if="imageData!=null">
+        <img class="preview" :src="picture">
+        <br>
+      <button @click="onUpload()">Upload</button>
+    </div> -->
+    <input type="file" @change="handleFileUpload($event)" />
+  <div v-if="image">
+    <img :src="image.src" />
+    </div>
+  	<br>
+	<button v-on:click="handleSubmit()">Submit</button>
+  </div>
                     </div>
                      <br>
                     </div>
@@ -75,7 +108,12 @@ export default{
         imageData: null,
         picture: null,
         uploadValue: 0,
-        url: ''
+        url: '',
+        file: "",
+      image: {},
+      user: {
+        type: '',
+      }
       }
     },
     methods:{
@@ -122,10 +160,83 @@ await axios.post('',this.url).then((res)=>{
                         })
                     }              
                 })    
+        },
+        handleFileUpload(event) {
+      this.file = event.target.files[0];
+    },
+
+    async handleSubmit() {
+				console.log(this.user)
+				console.log(this.file)
+        console.log("hLLOOO");
+        var contentTypeLocal
+
+        try{
+
+        contentTypeLocal = '';
+        if( this.user.type === "Image")
+        {
+        this.contentTypeLocal= 'image/png'
+        }
+        else if(this.user.type === "Audio")
+        {
+            this.contentTypeLocal= 'audio/mp3'
+        }
+        else if(this.user.type === "Video")
+        {
+            this.contentTypeLocal = "video/mp4"
+        }
+        var metaData = {
+
+        contentType: contentTypeLocal
+
         }
 
-  }
+        if(this.user.type !== "Text")
+        {
+        const storageRef = firebase.storage().ref();
+        console.log("Storage ref"+storageRef)
+
+        const imageRef = storageRef.child(`images/${this.file.name}`);
+
+        console.log("image ref"+this.imageRef)
+        console.log(metaData);
+
+        await imageRef.put(this.file, metaData);
+
+        const downloadUrl = await imageRef.getDownloadURL()
+
+        this.user.sourceUrl = downloadUrl
+        console.log(downloadUrl)
+        await axios.post('',this.downloadUrl).then((res)=>{
+                    if(res.data.status === 201){
+                        swal({
+                            text: "file uploaded",
+                            icon: 'success'
+                        }),
+                    this.$router.push({name: 'Home'})
+                    }
+                    else{
+                        swal({
+                            text: " File Not uploaded",
+                            icon: 'error'
+                        })
+                    }              
+                })   
+        }
+
+
+        }
+
+        catch(error){
+
+        console.log(error)
+
+        }
+      }
+    }
 }
+
 </script>
 
 
