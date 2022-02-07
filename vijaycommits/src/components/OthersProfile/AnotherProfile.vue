7225d501972bdfd97706fdd807058ac6ca7d88eb
damarla vijay Sai nag <64904540+the-damarla@users.noreply.github.com>
2022-02-07 19:59:50 +0530

    800pm

--- vijaycommits/src/components/OthersProfile/AnotherProfile.vue
+++ vijaycommits/src/components/OthersProfile/AnotherProfile.vue
@@ -7,7 +7,7 @@
 		<div class="profile">
             
 			<div class="profile-image">
-                <!-- <img :src="searchResult.url" alt="" height="250px" width="250px"> -->
+                <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" alt="" height="250px" width="250px">
 			</div>
 
 			<div class="profile-user-settings">
